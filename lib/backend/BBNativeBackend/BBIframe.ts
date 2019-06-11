bdea4269311b2c1cdf07b7ae91bc3e449d4c03e0
fadi426 <fadiqadadi@hotmail.com>
2019-06-11 13:25:42 +0200

    uploadIframe creation changes

--- lib/backend/BBNativeBackend/BBIframe.ts
+++ lib/backend/BBNativeBackend/BBIframe.ts
@@ -7,9 +7,29 @@ export default class BBIframe extends Iframe {
     public setUrl(parameter: string): Promise<string> {
         return new Promise((resolve, reject) => {
             this.iframe.setAttribute("src", parameter);
-            this.iframe.style.width = "0px";
-            this.iframe.style.height = "0px";
+
+            this.iframe.setAttribute("scrolling", "no"); 
+            this.iframe.style.width = "200px"; 
+            this.iframe.style.height = "200px";
+            this.iframe.style.position = "absolute";
+            this.iframe.style.top = "129px";
+            this.iframe.style.left = (screen.width - 118) / 2 + "px";
+            this.iframe.id = "upload-iframe";
             document.body.appendChild(this.iframe);
+
+            setTimeout(function(){  
+                var divs = this.iframe.contentDocument.getElementsByTagName("div"); 
+                var button = this.iframe.contentWindow.document.getElementById('newFile_chooseLocalFile');
+                for (var i = 0; i < divs.length; i++) { 
+                divs[i].style.display = 'none';         
+                } 
+                this.iframe.contentDocument.body.appendChild(button);
+                this.iframe.style.width = "118px";
+                this.iframe.style.height = "17px";
+                this.iframe.contentDocument.body.style.marginTop = "0px";
+                this.iframe.contentDocument.body.style.backgroundColor = "#ffffff";
+                this.iframe.contentDocument.body.style.background = "none";
+            }, 1000); 
 
             const result = "Url has been set";
             resolve(result);
@@ -39,7 +59,6 @@ export default class BBIframe extends Iframe {
     public submitAssignmentAttempt(): Promise<string> {
         return new Promise((resolve, reject) => {
             this.iframe.contentWindow.document.getElementsByClassName('submit button-1')[0].click();
-
             const result = "Assignment attempt has been submit";
             resolve(result);
         })
